f4e0846c39b14ac82f77516f6dfb10f0c5c72f2d
mklee <iamdevstar@gmail.com>
2024-11-18 20:22:31 +0900

    build: jdk version change 17 -> 21

--- src/main/java/com/vision/shoppingmall/controller/ProductController.java
+++ src/main/java/com/vision/shoppingmall/controller/ProductController.java
@@ -7,13 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/products")
 public class ProductController {
-//    public ProductController() {
-//
-//    }
 
     @GetMapping("")
-    public String listProduct() {
+    public String getProductList() {
         return "product/list"; // 경로가 정확히 맞는지 확인
     }
-
 }
